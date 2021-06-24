9f0c3ecc1c1c2a06d8fa200a0cc307d870c697b0
Bárbara Gomes <54336447+babigomes@users.noreply.github.com>
2021-06-23 23:54:45 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Código Para Todxs!</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="css/style.scss">  
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  </head>
+  <body>
+    <header class="headerContainer">
+        <img src="img/logo.png" alt="Logo" />
+        <p class="pHeader"> Programa Para Todxs</p>
+      </header>
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <div class="homepage">
+              <h1 class="titulo">Simular Crédito</h1><br>
+              <p>De quanto você precisa? </p>
+              <div class="slideContainer">
+                <p>R$ 1.000</p>
+                <input type="range" min="1" max="100" class="slider"><br>
+                <span>A partir de 36x de R$ 193.03</span>
+            </div><br>
+            <button class="buttonS">Simular</button>
+            </div>
+          </div>
+        </div>
+      </div>
+  </body>
+  <script src="js/javascript.js"></script>
+</html>
